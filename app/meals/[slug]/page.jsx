ace8a7008cf1b2f3f1c33c87a32d312b02ac2c1e
geroarmani <gerasimgrigorow@gmail.com>
2024-05-17 23:20:05 +0300

    Adding dynamic metadata

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -3,6 +3,19 @@ import classes from "./page.module.css";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }){
+  const meal = getMeal(params.slug)
+
+  if(!meal){
+    notFound()
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary
+  }
+}
+
 export default function SigleMeal({ params }) {
   const meal = getMeal(params.slug)
 
